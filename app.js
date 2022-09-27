f5c9caf221609a2a4578e4bf8d2bbca79fd746ff
Heather-Dee <hed609@gmail.com>
2022-09-27 19:53:29 -0400

    Current temp, city, description, humidity, wind elements

---
+++ app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
