4bf5cba11ca3a1af421a9c214faf128376b0e7dc
Heather-Dee <hed609@gmail.com>
2022-09-27 20:15:54 -0400

    Updated date and time format

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
+function formatDate(timestamp){
+    //calculate the date
+    let date = new Date(timestamp);
+    
+    //return time with 2 digits
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let minutes = date.getMinutes();
+    if (minutes < 10 ){
+        minutes = `0${minutes}`;
+    }
+
+    //return time with 2 digits
+
+    //return day of week not number
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+    //return day of week not number
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 
