e6ecd1e7fa0b49a06f2e73b5d6e499e66d116b42
Heather-Dee <hed609@gmail.com>
2022-10-04 17:35:27 -0400

    updated API Url with metric
    imperial was causing 404 error in console

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ forecastElement.innerHTML = forecastHTML;
 //pull weather data from Openweather API
 function getForecast(coordinates){
     let apiKey = "701f06352d61835bc4fc894e7b084629";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 
 
